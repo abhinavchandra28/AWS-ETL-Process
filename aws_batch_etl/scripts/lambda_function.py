c23e3f34a563686ee6d461776eb6faf53de18a75
Abhinav Chandra <65914399+abhinavchandra28@users.noreply.github.com>
2025-03-05 01:13:52 +0530

    Update lambda_function.py

--- aws_batch_etl/scripts/lambda_function.py
+++ aws_batch_etl/scripts/lambda_function.py
@@ -1,1 +1,7 @@
+import boto3
 
+glue_client = boto3.client("glue")
+
+def lambda_handler(event, context):
+    response = glue_client.start_job_run(JobName="batch-etl-job")
+    print(f"Triggered Glue Job: {response}")
